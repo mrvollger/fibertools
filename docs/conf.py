5affa4f1dd302a33d9b350eb3772b386ccc0c943
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-05-01 14:02:24 -0700

    .

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,13 @@ import fibertools
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
