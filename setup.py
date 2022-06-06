663c2201c8ca2761998046ee5c6df687267cce15
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-06-05 20:53:32 -0700

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -69,6 +69,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/mrvollger/fibertools",
-    version="0.1.9",
+    version="0.1.10",
     zip_safe=False,
 )
